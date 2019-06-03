3eb134270b23c19c33fbdb3568d9bf3d80ee0f1a
Rui-Jie Fang <rui.jie.fang@gmail.com>
2019-06-03 12:24:04 -0400

    finish problem 5: reverse a list

--- p5.ml
+++ p5.ml
@@ -1,5 +1,26 @@
 (* Problem 5. Reverse a list. *)
 
-let rev = function 
+open Printf
+
+(* returns a reversed list *) 
+let rec rev = function
   | [] -> []
-  | a :: b -> List.append (rev b) a
+  | a :: t -> (rev t) @ [a] 
+
+(* prints the list as input *)
+let rec print_list = function
+  | [] -> printf "\n" ; ()
+  | a :: t -> 
+    printf "%d " a ; print_list t
+
+(* a list *)
+let l = [1;2;3;4;5]
+
+(* the driver *)
+let () = 
+  printf "before reverse: " ;
+  print_list l ;
+  printf "after reverse: " ;
+  print_list (rev l)
+
+ 

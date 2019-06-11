e15a3de1397d26cfb8a9dce803657d7eeb66129d
Rui-Jie Fang <rfang@umd.edu>
2019-06-11 12:06:39 -0400

    p12: add vanilla implementation of ConstStream and correct first implementation

--- p12.ml
+++ p12.ml
@@ -15,10 +15,34 @@ type 'a rle =
     | One of 'a
     | Many of int * 'a
 
-let rec decode = fun l ->
-  let const_stream k = Stream.from (fun _ -> Some k) in
-    match l with
-      | [] -> []
-      | One a :: t -> [a] @ (decode t)
-      | Many (cnt, a) :: t -> (Stream.npeek cnt a) @ (decode t)
+(* decode using system's Stream module *)
+let rec decode = function
+  | [] -> []
+  | One a :: t -> [a] @ (decode t)
+  | Many (cnt, a) :: t -> 
+    (Stream.npeek cnt (
+             Stream.from (fun _ -> Some a))) @ (decode t)
 
+(* our vanilla implementation of a stream *)
+
+module ConstStream =
+  struct
+    type 'a stream  = Cons of 'a * 'a stream 
+    let create = fun n -> 
+      let rec f = Cons (n, f) in f
+    let head f = 
+      let Cons (n, _) = f in n
+    let tail f = 
+      let Cons (_, t) = f in t
+    let rec npeek f n = 
+      match n with
+        | 0 -> let Cons (a, _) = f in [a]
+        | k -> let Cons (a, _) = f in
+          a :: (npeek f (n - 1))
+  end
+
+let rec decode1 = function
+  | [] -> []
+  | One a :: t -> [a] @ (decode1 t)
+  | Many (cnt, a) :: t ->
+    (ConstStream.npeek (ConstStream.create a) cnt) @ (decode t)

83567600a412f0713502c1fb9705e1f0533b7c1a
Rui-Jie Fang <rfang@umd.edu>
2019-06-10 21:51:38 -0400

    complete p9 - turn consecutive elements into nested lists

--- p9.ml
+++ p9.ml
@@ -1,6 +1,7 @@
 (* Problem 9. Pack consecutive duplicates of list elements into sublists. (medium) *)
 
-Open Printf
+
+open Printf
 
 (* Example:
  pack ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"d";"e";"e";"e";"e"];;
@@ -11,6 +12,28 @@ Open Printf
 
 let rec pack = function
   | [] -> []
+  | [a] -> [[a]]
   | a :: (b :: _ as t) ->
     if a = b then 
-      
+      match pack t with
+        | [] -> [[a;b]]
+        | head :: tail ->
+          (a :: head) :: tail
+    else
+      let l = pack t in
+        [a] :: l
+
+let rec pprint_list = function
+  | [] -> ()
+  | [a] -> printf "%s];\n" a
+  | a :: t -> printf "%s, " a; pprint_list t
+
+let rec pprint_listlist = function
+  | [] -> ()
+  | [a] -> printf "[" ; pprint_list a ; ()
+  | a :: t -> printf "[" ; pprint_list a ; pprint_listlist t
+
+let () = 
+  let l = pack ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"d";"e";"e";"e";"e"] in
+    pprint_listlist l ;
+
